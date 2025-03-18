d9f97f6693a3a011a9048f04ba2029beb44441d1
MarcioWillian <00001133564756sp@al.educacao.sp.gov.br>
2025-03-18 07:44:52 -0300

    Add files via upload

---
+++ aula21.py
@@ -0,0 +1,13 @@
+# aula21.py
+
+#EXERCIOS DE FIXAÇAO
+
+# 1.adicione o numero 50 ao final da lista.
+lista_numeros = [10, 20, 30, 40]
+
+
+# 2. adicione "laranja" ao indice 1 da lista.
+lista_frutas = ["maça", "banana", "uva"]
+ 
+# 3. remove "cachorro" da lista.
+lista_animais = ["cachorro", "gato", "passaro", "cachorro"]
